021a2724a1cb31be2a5a5eaf54be010b67bd4a78
heymanshu13 <himanshurajputgwalior@gmail.com>
2026-09-24 12:59:08 +0000

    updated infra

--- infrastructure/terraform/modules/eks/outputs.tf
+++ infrastructure/terraform/modules/eks/outputs.tf
@@ -9,3 +9,13 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
+ 
+output "oidc_provider" {
+  description = "EKS OIDC provider"
+  value       = module.eks.oidc_provider
+}
+
+output "oidc_provider_arn" {
+  description = "EKS OIDC provider ARN"
+  value       = module.eks.oidc_provider_arn
+}
